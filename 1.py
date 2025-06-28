1eba2013217406ffc30424f2d91c1233ac0d7503
SUKRIT06-AI <sukrit.d6@gmail.com>
2025-06-28 11:06:16 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,26 @@
+print("Hello World")
+print("My name is Darsh,I was a Jee aspirant two years ago")
+print(90+20)
+print(10>11)
+print(10>12,12>11)
+print("HOW ARE YOU")
+print(1111*1111)
+print(123456*123456)
+print(1/2)
+print(322222222/1234556)
+a=10
+b=20
+print(a*b)
+print(a*b/b)
+c=2020
+print("I am Good in Physics,I like Physics",a+b+c)
+d=1234567890
+print(a+b)
+a=10
+b=30
+c=30
+d=40
+a1="Hello"
+b1="darsh"
+print(a1+" "+b1)
+print
